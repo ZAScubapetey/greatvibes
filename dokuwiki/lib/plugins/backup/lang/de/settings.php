67d3848295619f30cfe16294001472d40e499358
Lee Bezuidenhout <lee@framework.sh>
2021-11-23 15:26:32 +0200

    remove dokuwiki

--- dokuwiki/lib/plugins/backup/lang/de/settings.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$lang['backupnamespace'] = 'Media namespace (e.g. <code>wiki:backup</code>) in welchem die Backup Dateien gespeichert werden sollen.';
-$lang['filterdirs'] = 'Ausgeschlossene Verzeichnisse, durch Zeilenumbruch getrennt. Diese Verzeichnisse werden nicht in Ihren Backups enthalten sein.';
